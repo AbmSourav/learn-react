9de5218e961012528c8134e07881e8e68e629ae7
AbmSourav <keramotul.islam@gmail.com>
2018-12-10 18:57:16 +0600

    Events: Class base components. Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';   /* ES6 Destructuring */
 
-//== Types of components ==
+//Events. React's Synthetic Events. Class base component use case
+//more info: https://reactjs.org/docs/events.html
 
-//functional component
-/* const Header = () => {
-    return <h2>Hello from Header component</h2>
-} */
-
-//class base component
 class Header extends Component {
+
+    inputClickHandler() {
+        alert('Hello Universe');
+    }
+
+    onChangeHandler= (event) => {
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <header>
                 <h1>Logo</h1>
+                <button onClick = {this.inputClickHandler}>Greeting</button> <br /><br />
+
+                <input onChange = {this.onChangeHandler} type="text" />
             </header>
         )
     }

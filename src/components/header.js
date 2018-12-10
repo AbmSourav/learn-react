76a62b98fa6b8c64304bc4f24ea18be5bf7fdf3f
AbmSourav <keramotul.islam@gmail.com>
2018-12-10 20:16:35 +0600

    State. Class base component. Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';   /* ES6 Destructuring */
 
-//Events. React's Synthetic Events. Functional component use case
-//more info: https://reactjs.org/docs/events.html
+//State. Class base component use case
+//more info: https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
 
-const Header = () => {
+class Header extends Component {
 
-    function inputClickHandler(event) {
-        event.preventDefault();
-        alert('Hello Universe');
+    state = {
+        title: 'Hello Universe',
+        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
     }
 
-    function onChangeHandler(event) {
-        console.log(event.target.value);
+    onChangeHandler= (event) => {
+        this.setState( {
+            title: event.target.value
+        } );
     }
 
-    return (
-        <header>
-            <h1>Logo</h1>
-            <button onClick = {inputClickHandler}>Greeting</button> <br /><br />
+    render() {
+        return (
+            <header>
+                <h1>Logo</h1>
+                <h2>{this.state.title}</h2>
+                <p>{this.state.content}</p>
 
-            <input onChange = {onChangeHandler} type="text" />
-        </header>
-    );
+                <input onChange = {this.onChangeHandler} type="text" />
+            </header>
+        )
+    }
 }
+
+/* class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    render() {
+        return (
+        <div>
+            <h1>Hello, Universe</h1>
+            <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        </div>
+        );
+    }
+} */
 
 //available for other components
 export default Header;

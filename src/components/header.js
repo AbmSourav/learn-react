64f602dcbebb4aa7b327b851f61e15017c7c6ce8
AbmSourav <keramotul.islam@gmail.com>
2018-12-10 14:00:30 +0600

    Types of components. Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';   /* ES6 Destructuring */
 
-//component goes here
-const Header = () => {
+//== Types of components ==
+
+//functional component
+/* const Header = () => {
     return <h2>Hello from Header component</h2>
+} */
+
+//class base component
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Logo</h1>
+            </header>
+        )
+    }
 }
 
 //available for other components

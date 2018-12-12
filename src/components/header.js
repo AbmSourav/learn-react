c91b1945910dfd199f6594ddd35f36d8b57cc3d1
AbmSourav <keramotul.islam@gmail.com>
2018-12-12 15:20:59 +0600

    filtering on state. news app search field

--- src/components/header.js
+++ src/components/header.js
@@ -1,49 +1,17 @@
-import React, { Component } from 'react';   /* ES6 Destructuring */
+import React from 'react';
 
-//State. Class base component use case
-//more info: https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-class
-
-class Header extends Component {
-
-    state = {
-        title: 'Hello Universe',
-        content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-    }
-
-    onChangeHandler= (event) => {
-        this.setState( {
-            title: event.target.value
-        } );
-    }
-
-    render() {
-        return (
-            <header>
-                <h1>Logo</h1>
-                <h2>{this.state.title}</h2>
-                <p>{this.state.content}</p>
-
-                <input onChange = {this.onChangeHandler} type="text" />
-            </header>
-        )
-    }
+const Header = (props) => {
+    return (
+        <header>
+            <h1>News App</h1>
+            <h2>{}</h2>
+            <input 
+                onChange = {props.keywords} 
+                type="text" placeholder="Search News"
+            />
+        </header>
+    )
 }
-
-/* class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-
-    render() {
-        return (
-        <div>
-            <h1>Hello, Universe</h1>
-            <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-        </div>
-        );
-    }
-} */
 
 //available for other components
 export default Header;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,38 @@ import '../src/css/style.css'
 import JSON from './db.json' /* featching json file */
 
 //Components
-// import Header from './components/header'
+import Header from './components/header'
 import NewsList from './components/news_list'
 
 class App extends Component {   
     state = {
-        news: JSON
+        news: JSON,
+        filtered: []
+    }
+    getKeyWord = (event) => {
+        //console.log( event.target.value )
+        let keyword = event.target.value
+        let filtered = this.state.news.filter( (item) => {
+            return item.title.indexOf(keyword) > -1 /* filterin on news title. -1 makes it true */
+        } )
+        this.setState( {
+            filtered
+        } )
     }
 
     render() {
+        let newsFiltered = this.state.filtered
+        let newsAll = this.state.news
         return (
-            /* pasing data to the NewsList component through props(news) */
-            <div className="container">
-                <NewsList news = { this.state.news } />
+            <div>
+                <Header keywords={ this.getKeyWord } />
+
+                {/* pasing conditional data to the NewsList component through props(news) */}
+                <div className="container">
+                    <NewsList 
+                        news= { newsFiltered.length === 0 ? newsAll : newsFiltered } 
+                    />
+                </div>
             </div>
         )   
     }

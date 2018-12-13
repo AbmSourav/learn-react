f7aec6373d1b90ab5b4057ee25ec2c86512576ea
AbmSourav <keramotul.islam@gmail.com>
2018-12-13 13:45:27 +0600

    react router added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+// components
+import Home from './components/home'
+import Posts from './components/posts'
+import Profile from './components/profiles'
 
 const App = () => {
-    return <h1>Hello, Universe</h1>
+    return (
+        <BrowserRouter>
+            <div>
+
+                {/* e.x: https://facebook.com/ Or https://facebook.com/username that's what it's doing */}
+
+                {/* 'exact' means if it's http://localhost:3000/ only then Home component will be loaded, Otherwise something else. */}
+                <Route path="/" exact component={Home} />
+
+                {/* Posts component can be found on http://localhost:3000/posts . */}
+                <Route path="/posts" component={Posts} />
+
+                {/* Profile component can be found on http://localhost:3000/profiles */}
+                <Route path="/profiles" component={Profile} />
+                
+            </div>
+        </BrowserRouter>
+    )
 }
 
 ReactDOM.render(<App />, document.getElementById( 'root' ) );
